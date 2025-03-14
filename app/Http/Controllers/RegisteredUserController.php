6237d2c561bcda50814150392fa24354dc9e1c66
Rj <rjlebs1@gmail.com>
2025-03-14 23:16:47 +0800

    video 22_Make a Login and Registration System From Scratch: Part 2

--- app/Http/Controllers/RegisteredUserController.php
+++ app/Http/Controllers/RegisteredUserController.php
@@ -1,8 +1,12 @@
+
 <?php
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\Rules\Password;
 
 class RegisteredUserController extends Controller
 {
@@ -10,9 +14,20 @@ class RegisteredUserController extends Controller
     {
         return view('auth.register');
     }
-    
+
     public function store()
     {
-        dd(request()->all());
+        $attributes = request()->validate([
+            'first_name' => ['required'],
+            'last_name'  => ['required'],
+            'email'      => ['required', 'email'],
+            'password'   => ['required', Password::min(6), 'confirmed']
+        ]);
+
+        $user = User::create($attributes);
+
+        Auth::login($user);
+
+        return redirect('/jobs');
     }
 }
